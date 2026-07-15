57f756da9b718651dd7d5019e4ba693c7daa35be
Sajeeb Khan <sbhossain567@gmail.com>
2026-07-15 08:21:19 +0600

    Add initial implementation of price calculations for mangoes and oranges

---
+++ first.js
@@ -0,0 +1,22 @@
+const mangoPrice = 2000;
+const orangePrice = 200;
+const totalPrice = mangoPrice + orangePrice;
+//console.log(totalPrice);
+
+// Convert string to number
+const stringNumber = "67";
+const number = parseInt(stringNumber);
+//console.log( typeof number);
+
+// ARITHMETRIC OPERATOR
+const mangoQuantityInKg = 5;
+
+const substract = mangoPrice - orangePrice;
+
+const payableAmount = mangoPrice * mangoQuantityInKg;
+
+const perKgPrice = mangoPrice / mangoQuantityInKg;
+
+console.log(substract);
+console.log(payableAmount);
+console.log(perKgPrice);
