4f02bb082cde18ab4da2b9e6671363ce22067245
Sajeeb Khan <sbhossain567@gmail.com>
2026-07-16 00:18:44 +0600

    Add initial implementation of price calculations and conditional statements

---
+++ DAY-02/conditional.js
@@ -0,0 +1,45 @@
+const price1 = 320;
+const price2 = 220;
+
+// console.log("5" == 5);
+// console.log("5" === 5);
+
+// let isRaining = true;
+// if (isRaining) {
+//     console.log("Take an umbrella");
+// }
+
+// let stock = 10;
+// if(stock=== 0) console.log("Out of stock")
+
+// let number = 0;
+// if (number > 0) {
+//     console.log("It is positive");
+// } else {
+//     console.log("It is negative");
+// }
+
+
+// const year = 2005;
+// if (year % 4 === 0) {
+//     console.log("It is a leap year.");
+// } else {
+//     console.log("It is not a leap year.")
+// }
+
+
+// const speed = 90;
+// if (speed > 80) {
+//     console.log("Overspeeding");
+// } else {
+//     console.log("Normal speed");
+// }
+
+
+const age = 20;
+const hasTicket = false;
+if (age > 18 && hasTicket) {
+    console.log("Entry allowed");
+} else {
+    console.log("Entry not allowed");
+}
